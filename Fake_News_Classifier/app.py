153803014fa07691ed331921f697805226cc50af
Shubham Patil <81283032+ShubhLP@users.noreply.github.com>
2024-12-09 14:01:57 -0500

    Update app.py

--- Fake_News_Classifier/app.py
+++ Fake_News_Classifier/app.py
@@ -6,48 +6,38 @@ import joblib
 import numpy as np
 import os
 
-# Dynamically determine the base directory
 base_dir = os.path.dirname(os.path.abspath(__file__))
 
-# File paths
 vectorizer_file = os.path.join(base_dir, 'tfidf_vectorizer.json')
 model_file = os.path.join(base_dir, 'logistic_model.pkl')
 
-# Check if files exist
 if not os.path.exists(vectorizer_file):
     st.error(f"File not found: {vectorizer_file}")
 elif not os.path.exists(model_file):
     st.error(f"File not found: {model_file}")
 else:
-    # Load the vectorizer parameters
     with open(vectorizer_file, 'r') as f:
         vectorizer_params = json.load(f)
 
-    # Recreate the TfidfVectorizer using the loaded parameters
     vectorizer = TfidfVectorizer(
         vocabulary=vectorizer_params['vocabulary'],
         stop_words=vectorizer_params['stop_words'],
         max_features=vectorizer_params['max_features']
     )
 
-    # Manually set the idf_ attribute
     vectorizer.idf_ = np.array(vectorizer_params['idf_'])
 
-    # Load the pre-fitted model
     model = joblib.load(model_file)
 
-    # Streamlit app layout
     st.title("News Article Classifier")
     user_input = st.text_area("Enter News Article:")
 
     if st.button("Check News"):
-        if user_input.strip():  # Check if input is not empty
+        if user_input.strip():
             try:
-                # Transform the user input using the fitted vectorizer
                 processed_input = vectorizer.transform([user_input])
                 st.write(f"Processed input shape: {processed_input.shape}")
 
-                # Predict using the model
                 prediction = model.predict(processed_input)
 
                 if prediction[0] == 1:

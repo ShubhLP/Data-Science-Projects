94c9bee0d869b21f47cc2ee8f53503b9e174b088
Shubham Patil <81283032+ShubhLP@users.noreply.github.com>
2025-01-02 11:58:46 -0500

    app.py added

---
+++ Credit_Scoring_and_Loan_Default_Prediction/app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import numpy as np
+import joblib
+
+model = joblib.load('logistic_model.pkl')
+
+risks = {0: 'Low', 1: 'Medium', 2: 'High'}
+
+st.title("Customer Risk Prediction App")
+st.write("Classify customers into risk categories based on their profile.")
+
+credit_per_month_risk = st.slider("Credit Per Month Risk (0: Low, 1: Medium, 2: High)", 0, 2, 1)
+credit_util_score = st.slider("Credit Utilization Score (0: Low, 1: High)", 0.0, 1.0, 0.5, step=0.1)
+housing_risk = st.slider("Housing Risk (0: Low, 1: High)", 0, 1, 1)
+savings_risk = st.slider("Savings Risk (0: Low, 1: High)", 0, 1, 1)
+
+if st.button("Predict Risk"):
+    input_features = np.array([[credit_per_month_risk, credit_util_score, housing_risk, savings_risk]])
+    
+    predicted_risk = model.predict(input_features)
+    predicted_risk_mapped = risks[predicted_risk[0]]
+    
+    st.success(f"Predicted Risk Category: {predicted_risk[0]} - {predicted_risk_mapped}")
